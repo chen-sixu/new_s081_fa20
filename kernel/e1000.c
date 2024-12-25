8c9a03f0ebb11e2e9589d4ecd43ff667862a3b5b
sixu_chen <csx0129@gmail.com>
2024-12-25 21:06:26 +0800

    lab net: NIC driver

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,27 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
+  acquire(&e1000_lock);
+  uint64 index = regs[E1000_TDT];
+  if (!(tx_ring[index].status & E1000_TXD_STAT_DD)) // havn't finished sending
+  {    
+	release(&e1000_lock);
+	return -1;
+  }
+
+  // free the current mbuf
+  if (tx_mbufs[index])
+	mbuffree(tx_mbufs[index]);
   
+  // add the new mbuf to the ring
+  tx_mbufs[index] = m;
+  tx_ring[index].addr = (uint64) m->head;
+  tx_ring[index].length = m->len;
+  tx_ring[index].cmd = E1000_TXD_CMD_RS | E1000_TXD_CMD_EOP;
+  regs[E1000_TDT] = (index + 1) % TX_RING_SIZE;
+
+  release(&e1000_lock);
+
   return 0;
 }
 
@@ -115,6 +135,21 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  while (1){
+  	uint64 index = regs[E1000_RDT];
+	index = (index + 1) % RX_RING_SIZE; // next packet
+  	if (!(rx_ring[index].status & E1000_RXD_STAT_DD)) { // havn't finished receiving
+	  return;
+  	}
+	rx_mbufs[index]->len = rx_ring[index].length;
+	net_rx(rx_mbufs[index]);
+	rx_mbufs[index] = mbufalloc(0);
+	rx_ring[index].addr = (uint64) rx_mbufs[index]->head;
+	rx_ring[index].status = 0;
+	regs[E1000_RDT] = index;
+
+  }
+
 }
 
 void
